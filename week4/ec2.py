a6bbf38aff4fce1a2be129aa4f28fb446c621df3
Fabian Reyes <fabianreyes@Fabians-MacBook-Air.local>
2026-09-23 14:11:38 -0500

    Last update of ec2.py

--- week4/ec2.py
+++ week4/ec2.py
@@ -1,9 +1,9 @@
 import boto3
-import json
 
-def get_latest_amazon_linux_2_ami():
-    client = boto3.client('ec2')
+DRYRUN = False # Set to True to perform a dry run without actually creating the instance
 
+def get_image(ec2_client): # Function to get the latest Amazon Linux 2 AMI ID
+    
     Filters = [
         {
             'Name': 'description',
@@ -19,53 +19,73 @@ def get_latest_amazon_linux_2_ami():
         }
     ]
 
-    image_data = client.describe_images(Filters=Filters)
+    image_data = ec2_client.describe_images(Filters=Filters)
 
     image_id = image_data['Images'][0]['ImageId']
     #print(f"Image ID: {image_id}")
     return image_id
 
-def create_ec2_instance(image_id):  
-    ec2 = boto3.client('ec2')
+def create_ec2(image_id, ec2_client):  # Function to create an EC2 instance
 
-    response = ec2.run_instances(
+    response = ec2_client.run_instances(
         ImageId=image_id,
         InstanceType='t2.micro',
         MinCount=1,
         MaxCount=1,
-        DryRun=False # Set tp False to actually create the instance
+        DryRun=DRYRUN # Set to False to actually create the instance
     )
 
     #print(response['Instances'][0]['InstanceId'])
     #print(f"EC2 instance created with ID: {instance_id}")
-    return response['Instances'][0]['InstanceId']  # Return the instance object
+    instance_id = response['Instances'][0]['InstanceId'] #Get the instance ID from the AWS response
+    return instance_id   
 
-def print_instance_details(instance):
+def print_instance_details(instance): #Information about the EC2 instance
 
     print(f"Instance ID: {instance.id}")
     print(f"Instance State: {instance.state['Name']}")
     print(f"Public DNS: {instance.public_dns_name}")
     print(f"Public IP: {instance.public_ip_address}")
-    
+    print(f"Tags: {instance.tags}") # Print the tags
     
 def main():
-    image_id = get_latest_amazon_linux_2_ami()
+
+    ec2_client = boto3.client('ec2') # Create an EC2 client
+    image_id = get_image(ec2_client) # Find the latest Amazon Linux 2 AMI ID
+
     print(f"Latest Amazon Linux 2 AMI ID retrieved successfully.") 
     print(f"Image ID: {image_id}")
 
-    instance_id = create_ec2_instance(image_id) #create the instance and get the instance ID
+    instance_id = create_ec2(image_id, ec2_client) # Create the instance and get the instance ID
+
     print(f"EC2 instance created with ID: {instance_id}")
 
-    ec2 = boto3.resource('ec2')
+    ec2 = boto3.resource('ec2') # Create an EC2 resource object
     instance = ec2.Instance(instance_id)  # Create an instance object using the instance ID
 
     instance.wait_until_running() # Wait until the instance is running
+    instance.reload()
+
+    print_instance_details(instance)   # ← tags should be []
+
+    instance.create_tags( # Add tags to the instance
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'Fabian'
+            }
+        ]
+    )
+
     instance.reload()  # Refresh the instance attributes
-    print_instance_details(instance) #print the instance details
+
+    print_instance_details(instance) #print the instance details and new tag
 
     print("Instance is running and details are updated.")
+
     instance.terminate()
     instance.wait_until_terminated()  # Wait until the instance is terminated
+
     print(f"EC2 instance with ID {instance.id} has been terminated.")
 
 
